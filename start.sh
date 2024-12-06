d8845305a841a5bf66ed910d91074d174852f121
Federico Rossi <federico.rossi@ing.unipi.it>
2024-12-06 10:18:29 +0100

    Updated start script using different name for mongodb data and container

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 # Step 0: stop active containers
-docker stop mongodb pf-api
+docker stop pf-db pf-api
 
 # Step 1: Create a Docker volume for MongoDB data persistence
-docker volume create mongodb-data
+docker volume create pf-db-data
 
 # Step 2: Create a network for the containers
 docker network create pf-network
@@ -10,10 +10,10 @@ docker network create pf-network
 # Step 3: Run the MongoDB container with persistent storage
 docker run -d \
   --rm \
-  --name mongodb \
+  --name pf-db \
   --network pf-network \
   -p 27017:27017 \
-  -v mongodb-data:/data/db \
+  -v pf-db-data:/data/db \
   -e MONGO_INITDB_ROOT_USERNAME=root \
   -e MONGO_INITDB_ROOT_PASSWORD=example \
   mongo:6.0
@@ -22,12 +22,12 @@ docker run -d \
 docker build -t pf-api .
 
 # Step 5: Run the pf-api container
-docker run -d \
+docker run -d  \
   --rm \
   --name pf-api \
   --network pf-network \
   -p 8000:8000 \
   -v "${PWD}/static:/pfapi/static/" \
-  -e MONGO_URI=mongodb://root:example@mongodb:27017 \
+  -e MONGO_URI=mongodb://root:example@pf-db:27017 \
   -e MONGO_DB=pf-api \
   pf-api
