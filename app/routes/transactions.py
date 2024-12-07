f9a89b98635231b9984d4401c146bf5151f19898
Federico Rossi <federico.rossi95@outlook.it>
2024-12-07 11:57:49 +0100

    Added planned+actuale income to expenses page

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -73,7 +73,7 @@ async def overview_transactions(
                     "$gte": datetime(year, 1, 1),
                     "$lt": datetime(year + 1, 1, 1),
                 },
-                "amount": {"$lt": 0}
+                #"amount": {"$lt": 0}
 
             }
         },
